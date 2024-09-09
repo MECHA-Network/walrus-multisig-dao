c5d523663a54f1a761ba30b5cf46cac4c33aefcf
Tahlil <bsse0803@iit.du.ac.bd>
2024-09-10 03:06:51 +0600

    Add password ui

---
+++ src/pages/components/Auth/index.tsx
@@ -0,0 +1,81 @@
+import './index.css';
+interface CustomCSSProperties extends React.CSSProperties {
+    '--i': string;
+}
+
+export const Auth = () => {
+    return (
+        <div className="container">
+        <div className="login-box">
+            <h2>Login</h2>
+            <form action="#">
+                <div className="input-box">
+                    <input type="email" required />
+                    <label>Email</label>
+                </div>
+                <div className="input-box">
+                    <input type="password" required />
+                    <label>Password</label>
+                </div>
+                <div className="forgot-pass">
+                    <a href="#">Forgot your password?</a>
+                </div>
+                <button type="submit" className="btn">Login</button>
+                <div className="signup-link">
+                    <a href="#">Signup</a>
+                </div>
+            </form>
+        </div>
+        <span style={{ '--i': '0' } as CustomCSSProperties}></span>
+        <span style={{ '--i': '1' } as CustomCSSProperties}></span>
+        <span style={{ '--i': '2' } as CustomCSSProperties}></span>
+        <span style={{ '--i': '3' } as CustomCSSProperties}></span>
+        <span style={{ '--i': '4' } as CustomCSSProperties}></span>
+        <span style={{ '--i': '5' } as CustomCSSProperties}></span>
+        <span style={{ '--i': '6' } as CustomCSSProperties}></span>
+        <span style={{ '--i': '7' } as CustomCSSProperties}></span>
+        <span style={{ '--i': '8' } as CustomCSSProperties}></span>
+        <span style={{ '--i': '9' } as CustomCSSProperties}></span>
+        <span style={{ '--i': '10' } as CustomCSSProperties}></span>
+        <span style={{ '--i': '11' } as CustomCSSProperties}></span>
+        <span style={{ '--i': '12' } as CustomCSSProperties}></span>
+        <span style={{ '--i': '13' } as CustomCSSProperties}></span>
+        <span style={{ '--i': '14' } as CustomCSSProperties}></span>
+        <span style={{ '--i': '15' } as CustomCSSProperties}></span>
+        <span style={{ '--i': '16' } as CustomCSSProperties}></span>
+        <span style={{ '--i': '17' } as CustomCSSProperties}></span>
+        <span style={{ '--i': '18' } as CustomCSSProperties}></span>
+        <span style={{ '--i': '19' } as CustomCSSProperties}></span>
+        <span style={{ '--i': '20' } as CustomCSSProperties}></span>
+        <span style={{ '--i': '21' } as CustomCSSProperties}></span>
+        <span style={{ '--i': '22' } as CustomCSSProperties}></span>
+        <span style={{ '--i': '23' } as CustomCSSProperties}></span>
+        <span style={{ '--i': '24' } as CustomCSSProperties}></span>
+        <span style={{ '--i': '25' } as CustomCSSProperties}></span>
+        <span style={{ '--i': '26' } as CustomCSSProperties}></span>
+        <span style={{ '--i': '27' } as CustomCSSProperties}></span>
+        <span style={{ '--i': '28' } as CustomCSSProperties}></span>
+        <span style={{ '--i': '29' } as CustomCSSProperties}></span>
+        <span style={{ '--i': '30' } as CustomCSSProperties}></span>
+        <span style={{ '--i': '31' } as CustomCSSProperties}></span>
+        <span style={{ '--i': '32' } as CustomCSSProperties}></span>
+        <span style={{ '--i': '33' } as CustomCSSProperties}></span>
+        <span style={{ '--i': '34' } as CustomCSSProperties}></span>
+        <span style={{ '--i': '35' } as CustomCSSProperties}></span>
+        <span style={{ '--i': '36' } as CustomCSSProperties}></span>
+        <span style={{ '--i': '37' } as CustomCSSProperties}></span>
+        <span style={{ '--i': '38' } as CustomCSSProperties}></span>
+        <span style={{ '--i': '39' } as CustomCSSProperties}></span>
+        <span style={{ '--i': '40' } as CustomCSSProperties}></span>
+        <span style={{ '--i': '41' } as CustomCSSProperties}></span>
+        <span style={{ '--i': '42' } as CustomCSSProperties}></span>
+        <span style={{ '--i': '43' } as CustomCSSProperties}></span>
+        <span style={{ '--i': '44' } as CustomCSSProperties}></span>
+        <span style={{ '--i': '45' } as CustomCSSProperties}></span>
+        <span style={{ '--i': '46' } as CustomCSSProperties}></span>
+        <span style={{ '--i': '47' } as CustomCSSProperties}></span>
+        <span style={{ '--i': '48' } as CustomCSSProperties}></span>
+        <span style={{ '--i': '49' } as CustomCSSProperties}></span>
+    </div>
+    );
+}
--- src/pages/panel/Panel.tsx
+++ src/pages/panel/Panel.tsx
@@ -2,6 +2,7 @@ import '@pages/panel/Panel.css';
 import React, { ReactElement, useEffect, useState } from 'react';
 import { APP_COLLAPSE_WIDTH, APP_EXTEND_WIDTH, URLS } from './const';
 import classNames from 'classnames';
+import { Auth } from "../components/Auth";
 import Button from './Button';
 export default function Panel(): JSX.Element {
   const [enabled, setEnabled] = useState(false);
@@ -18,73 +19,81 @@ export default function Panel(): JSX.Element {
     setEnabled(newValue);
     handleOnToggle(newValue);
   }
+  let loggedIn = false;
   
-
-  return (
-    <div>
-      <div className='bg-blue-500 h-screen text-white p-12 mr-500px'>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis autem reprehenderit enim. Recusandae porro eaque maiores. Eaque cupiditate inventore ut soluta fuga quis tempora autem excepturi nulla laudantium! Unde tenetur, corporis quasi beatae voluptatum laudantium ducimus rerum eum obcaecati, quibusdam pariatur qui consequuntur culpa, inventore mollitia ad nesciunt ipsam facere sint! Esse eius deleniti, eaque quia, ex ullam consequuntur sunt a recusandae voluptatibus neque velit consectetur assumenda est odit laboriosam quae similique ducimus, veniam hic. Culpa odio cumque dolorem et ut quis ipsum deleniti, quisquam modi ducimus iusto hic sequi perspiciatis quibusdam voluptate dicta illum vel rem, quasi nostrum voluptatibus!
-    </div>
-   <div
-    style={{
-      width: sidePanelWidth - 5,
-      boxShadow: '0px 0px 5px #0000009e',
-    }}
-    className="absolute top-0 right-0 bottom-0 z-max bg-[#F5F8FA] ease-in-out duration-300 overflow-hidden"
-  >
-  
-    <iframe
-      className={classNames('absolute w-full h-full border-none ease-linear overflow-hidden', {
-        'opacity-0': !enabled,
-        '-z-10': !enabled,
-      })}
-      title={URLS[tabIndex].name}
-      src={URLS[tabIndex].url}
-    />
-    <div
-      className={classNames('absolute h-full flex border-none flex-col ease-linear w-[50px] space-y-3 p-1', {
-        'opacity-0': enabled,
-        '-z-10': enabled,
-      })}
+  if (!loggedIn) {
+    return (
+      <Auth/>
+    )
+  }
+  else{
+    return (
+      <div>
+        <div className='bg-blue-500 h-screen text-white p-12 mr-500px'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis autem reprehenderit enim. Recusandae porro eaque maiores. Eaque cupiditate inventore ut soluta fuga quis tempora autem excepturi nulla laudantium! Unde tenetur, corporis quasi beatae voluptatum laudantium ducimus rerum eum obcaecati, quibusdam pariatur qui consequuntur culpa, inventore mollitia ad nesciunt ipsam facere sint! Esse eius deleniti, eaque quia, ex ullam consequuntur sunt a recusandae voluptatibus neque velit consectetur assumenda est odit laboriosam quae similique ducimus, veniam hic. Culpa odio cumque dolorem et ut quis ipsum deleniti, quisquam modi ducimus iusto hic sequi perspiciatis quibusdam voluptate dicta illum vel rem, quasi nostrum voluptatibus!
+      </div>
+     <div
+      style={{
+        width: sidePanelWidth - 5,
+        boxShadow: '0px 0px 5px #0000009e',
+      }}
+      className="absolute top-0 right-0 bottom-0 z-max bg-[#F5F8FA] ease-in-out duration-300 overflow-hidden"
     >
-      {URLS.map(({ name, image }, _index) => {
-        function onMenuClick(index: number) {
-          setTabIndex(index);
-          openPanel(true);
-        }
-        return (
-          <Button active={_index === tabIndex} onClick={() => onMenuClick(_index)} className="py-2">
-            <img src={image} className="w-full" />
-          </Button>
-        );
-      })}
+    
+      <iframe
+        className={classNames('absolute w-full h-full border-none ease-linear overflow-hidden', {
+          'opacity-0': !enabled,
+          '-z-10': !enabled,
+        })}
+        title={URLS[tabIndex].name}
+        src={URLS[tabIndex].url}
+      />
+      <div
+        className={classNames('absolute h-full flex border-none flex-col ease-linear w-[50px] space-y-3 p-1', {
+          'opacity-0': enabled,
+          '-z-10': enabled,
+        })}
+      >
+        {URLS.map(({ name, image }, _index) => {
+          function onMenuClick(index: number) {
+            setTabIndex(index);
+            openPanel(true);
+          }
+          return (
+            <Button active={_index === tabIndex} onClick={() => onMenuClick(_index)} className="py-2">
+              <img src={image} className="w-full" />
+            </Button>
+          );
+        })}
+      </div>
+      <div className="absolute bottom-0 left-0 w-[50px] z-10 flex justify-center items-center p-1">
+        <Button active={enabled} onClick={() => openPanel()}>
+          <span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d={
+                  enabled
+                    ? 'M9 9V4.5M9 9H4.5M9 9L3.75 3.75M9 15v4.5M9 15H4.5M9 15l-5.25 5.25M15 9h4.5M15 9V4.5M15 9l5.25-5.25M15 15h4.5M15 15v4.5m0-4.5l5.25 5.25'
+                    : 'M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15'
+                }
+              />
+            </svg>
+          </span>
+        </Button>
+      </div>
     </div>
-    <div className="absolute bottom-0 left-0 w-[50px] z-10 flex justify-center items-center p-1">
-      <Button active={enabled} onClick={() => openPanel()}>
-        <span>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d={
-                enabled
-                  ? 'M9 9V4.5M9 9H4.5M9 9L3.75 3.75M9 15v4.5M9 15H4.5M9 15l-5.25 5.25M15 9h4.5M15 9V4.5M15 9l5.25-5.25M15 15h4.5M15 15v4.5m0-4.5l5.25 5.25'
-                  : 'M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15'
-              }
-            />
-          </svg>
-        </span>
-      </Button>
-    </div>
-  </div>
-    </div>
- 
-  );
+      </div>
+   
+    );
+  }
+  
 }
